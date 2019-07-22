1ea8d7ccfef329246fd80318011c17f91b1041cd
Amir Hassan Mousavi <riki.devdroid@gmail.com>
2019-07-22 19:25:43 +0430

    Design

---
+++ app/src/main/java/com/example/amir/base/MVVM/views/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.amir.base.MVVM.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+import com.example.amir.base.R;
+
+public class SplashActivity extends Activity{
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, SigninActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
